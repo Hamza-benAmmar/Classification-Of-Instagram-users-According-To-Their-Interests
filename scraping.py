0d82b61465ab334d457ad9e3c8d0ed1affc44210
Hamza-BenAmmar <hamza.benammar@insat.ucar.tn>
2024-04-24 18:59:05 +0100

    scraping

--- scraping.py
+++ scraping.py
@@ -1,23 +1,27 @@
-from datetime import datetime
 import os
 import requests
+from datetime import datetime
 import json
 
+# Load Instagram API configuration
 with open('config.json', 'r') as f:
     config = json.load(f)
 
 instagram_account_id = config['instagram_account_id']
 access_token = config['access_token']
 
+# Function to create folder if not exists
 def create_folder_if_not_exists(folder_path):
     if not os.path.exists(folder_path):
         os.makedirs(folder_path)
 
+# Function to save images to folder
 def save_images_to_folder(user_data):
     username = user_data['username']
     user_folder = os.path.join('images', username)
     create_folder_if_not_exists(user_folder)
-# if media type is video , we download the thumbnail_url else we download the media_url
+    
+    # Save images
     for post in user_data['posts']:
         media_type = post['media_type']
         media_url = post.get('media_url', '') 
@@ -27,7 +31,6 @@ def save_images_to_folder(user_data):
         else:
             image_url = media_url
 
-        # Download the image if 'image_url' is not empty
         if image_url:
             timestamp = datetime.strptime(post['timestamp'], "%Y-%m-%dT%H:%M:%S+0000")
             timestamp_seconds = int(timestamp.timestamp())
@@ -37,8 +40,7 @@ def save_images_to_folder(user_data):
         else:
             print("Warning: 'media_url' not found for a post.")
 
-
-# get user info and posts
+# Function to get user info and posts
 def get_user_info_and_posts(username, instagram_account_id, access_token):
     ig_params = {
         'fields': 'business_discovery.username(' + username + '){followers_count,media_count,name,media.limit(100){media_type,thumbnail_url,media_url,timestamp,caption,comments_count,like_count}}',
@@ -48,7 +50,7 @@ def get_user_info_and_posts(username, instagram_account_id, access_token):
     response = requests.get(endpoint, params=ig_params)
     return format_response(response.json())
 
-# formatting the response
+# Function to format the response
 def format_response(response):
     user = {}
     try:
@@ -63,6 +65,20 @@ def format_response(response):
         print("KeyError:", e)
     return user
 
+# Load usernames and interests from the JSON file
+with open('output_interests.json', 'r') as f:
+    interests_data = json.load(f)
+
+# Get user info and posts for each username
 users = []
-users.append(get_user_info_and_posts('arianagrande', instagram_account_id, access_token))
+for username, interests in interests_data.items():
+    user_data = get_user_info_and_posts(username, instagram_account_id, access_token)
+    user_data['interests'] = interests
+    print(user_data)
+    users.append(user_data)
+
+# Print or do whatever you want with the users data
 print(users)
+
+
+
